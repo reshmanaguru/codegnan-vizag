01cb7cec9e76554f5c76f4c80641d69bfe2f6066
reshmanaguru <reshmanaguru30@gmail.com>
2026-05-13 15:44:56 +0530

    Add files via upload
    Day 1 basics :
    Introduction of python
    Tokens
    Indentifiers
    Variables

---
+++ Day 1.py
@@ -0,0 +1,68 @@
+#print("This my first python class")
+''''
+
+What is python:
+    
+    i)high level language:
+        
+        no need to worry about the low level maintenance
+
+    ii)dynamically typed language:
+
+        no need to mention the kind of data taking to variable
+
+
+        eg:num=23
+        so="Python"
+        any=[1,2]
+        an={"Name";"Reshma"}
+        print(num)
+        print(so)
+        print(any)
+        print(an)
+
+
+    iii)interperter language:
+        
+        runs the code in line by line
+
+Tokens:
+
+    keywords in the program
+
+      if,else,while,for
+      
+Identifiers:
+
+    name of variables,function and classes
+
+variables:
+
+    good ways: 
+
+        A-Z,a-z,_
+
+    bad ways: when ever we ue these we will get errors
+
+        special char,space,0-9
+        
+'''
+0name="Reshma"
+& roll no=[47]
+l ist=[1,_2]
+name0="Reshma"
+roll no=47
+list=[1,2]
+print(name)
+print(roll_no)
+print(list)
+
+
+        
+        
+
+        
+        
+
+        
+    
