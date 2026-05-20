9eb9ed77c89914e86dd58365b8ddad88fa9ef6cd
reshmanaguru <reshmanaguru30@gmail.com>
2026-05-20 15:25:33 +0530

    Add files via upload
    Day 7
    topics covered:
    1)conditional statements
    2)F string

---
+++ Day7.py
@@ -0,0 +1,186 @@
+F-STRING or DOC-STRING:
+
+-------------------------------------
+
+num = 10
+if num%2 == 0:
+    print(f"{num}is a even number")-->10is a even number
+-------------------------------------------------------
+
+statements:
+    statements are of three types
+    1)conditional statement-->(if,else,elif)
+    2)control floe statement-->(break,continue,pass)
+    3)loop statement-->(for,while)
+
+1)conditional statements:
+
+---------------------------
+
+1) if :--> to check the statement is true or not
+
+--------------
+num = 6
+if num%2==0:
+    print("even")-->even
+
+2)if-else--> else in the if statement, incase the condition becomes false then it will enter into fall-back(else), it will execute whatever inside i
+
+---------------------------------------------------------
+program to print whether number is even or odd
+---------------------------------------------------------
+num = 7
+if(num%2==0):
+    print(f"{num} is even number")
+else:
+    print(f"{num} is odd number")
+---------------------------------------------------------
+
+program to print voting
+---------------------------------------------------------
+
+age = int(inout("enter your age:")
+if age>=18:
+    print("eligible to vote")
+else:
+    print(f"you have to wait for (18-age)more years")
+---------------------------------------------------------
+
+program for greater number
+---------------------------------------------------------
+num = 8
+num_2 = 15
+if num>= num_2:
+    print(f"{num}is greater number than {num_2}")
+else:
+    print(f"{num} is greater number than {num}")
+
+----------------------------------------------------------
+
+program for leap year
+----------------------------------------------------------
+year_= 2000
+if (year% 4 == 0 and year% 100 ! = 0) or year% 400 == o;
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+
+
+program to print whether vowel or consonent
+----------------------------------------------------------
+
+vowel ="a"
+if vowel in"AEIOUaeiou":
+    print(f"{vowel} is a consonents")
+else:
+    print(f"{vowel} is a consonents")
+
+----------------------------------------------------------
+
+program to print positive no or negative
+----------------------------------------------------------
+num = -9
+if num>= 0:
+    print(f"{num} is a positive number")
+else:
+    print(f"{num} is a negative number")
+
+-----------------------------------------------------------
+
+program to print pass or fail
+-----------------------------------------------------------
+
+marks = int(input("Enter your marks:"))
+name = input()
+if marks>=45:
+   print(f"{name} is pass")
+else:
+    print(f"{name} is fail")
+
+program for divisibility 
+-----------------------------------------------------------
+
+num = 76
+
+if num % 3 ==0 and num % 5 == 0:
+    print(f"{num} is divi by 3 and 5")
+else:
+    print(f"{num} not")
+
+-------------------------------------------------------------
+
+program for traffic signals
+
+-------------------------------------------------------------
+signal = int(input("enter \n1.red \n2.green:"))
+if signal == 1:
+    print("pls stop")
+else:
+    print("go")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+    
+
+    
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
